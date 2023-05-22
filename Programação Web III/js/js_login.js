0625d2e000f44eb0b16dc344f3b5349577ad5d65
esthermoraes <esther.ifes2021@gmail.com>
2023-05-22 12:41:33 -0300

    Trabalho 2 em andamento

--- Programação Web III/js/js_login.js
+++ Programação Web III/js/js_login.js
@@ -1,36 +1,33 @@
-function exibirCaluno(){
-    let btn_aluno = document.getElementById('login');
-
-    btn_aluno.addEventListener('click', function() {
-        let containerA = document.getElementById('divLogin');
-        let containerP = document.getElementById('divCadastro');
+var divCadastro = document.getElementById('divCadastro');
+var divLogin = document.getElementById('divLogin');  
+var divRecuperar = document.getElementById('divRecuperar');
 
 
-        if (containerA.style.display != 'block') {
-            containerA.style.display = 'block';
-            containerP.style.display = 'none';
-            return;
-        }
-        else {
-            containerA.style.display = 'none';
-        }
-    })
-};
 
-function exibirCprofessor(){
-    let btn_professor = document.getElementById('cadastro');
+function exibirpadrao(){  
+    
+    divLogin.style.display = 'block';
+    divCadastro.style.display = 'none';
+    divRecuperar.style.display = 'none';
 
-    btn_professor.addEventListener('click', function() {
-        let containerP = document.getElementById('divCadastro');
-        let containerA = document.getElementById('divLogin');
+}
 
-        if (containerP.style.display != 'block') {
-            containerP.style.display = 'block';
-            containerA.style.display = 'none';
-            return;
-        }
-        else {
-            containerP.style.display = 'none';
-        }
-    })
-};
+function exibirLogin(){
+    divLogin.style.display = 'block';
+    divCadastro.style.display = 'none';
+    divRecuperar.style.display = 'none';
+
+}
+
+function exibirCadastro(){
+    divLogin.style.display = 'none';
+    divCadastro.style.display = 'block';
+    divRecuperar.style.display = 'none';
+}
+
+function exibirRecuperar(){
+    console.log("testando");
+    divLogin.style.display = 'none';
+    divCadastro.style.display = 'none';
+    divRecuperar.style.display = 'block';
+}
