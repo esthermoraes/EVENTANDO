a77b635b8b3720a4574c519cf2bfbf13b42a8a9b
esthermoraes <esther.ifes2021@gmail.com>
2023-05-22 11:03:38 -0300

    atualizações

---
+++ Programação Web III/js/js_login.js
@@ -0,0 +1,36 @@
+function exibirCaluno(){
+    let btn_aluno = document.getElementById('login');
+
+    btn_aluno.addEventListener('click', function() {
+        let containerA = document.getElementById('divLogin');
+        let containerP = document.getElementById('divCadastro');
+
+
+        if (containerA.style.display != 'block') {
+            containerA.style.display = 'block';
+            containerP.style.display = 'none';
+            return;
+        }
+        else {
+            containerA.style.display = 'none';
+        }
+    })
+};
+
+function exibirCprofessor(){
+    let btn_professor = document.getElementById('cadastro');
+
+    btn_professor.addEventListener('click', function() {
+        let containerP = document.getElementById('divLogin');
+        let containerA = document.getElementById('divCadastro');
+
+        if (containerP.style.display != 'block') {
+            containerP.style.display = 'block';
+            containerA.style.display = 'none';
+            return;
+        }
+        else {
+            containerP.style.display = 'none';
+        }
+    })
+};
